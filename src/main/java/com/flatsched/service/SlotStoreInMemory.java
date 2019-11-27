5a55376da77b365783d48aed046a259813a0c62d
Sergey Gordienko <sergey.gordienko@clarivate.com>
2019-11-27 08:40:55 +0300

    Concurrent code correction

--- src/main/java/com/flatsched/service/SlotStoreInMemory.java
+++ src/main/java/com/flatsched/service/SlotStoreInMemory.java
@@ -13,7 +13,6 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
 
 @Singleton
 public class SlotStoreInMemory implements SlotStore {
@@ -30,33 +29,39 @@ public class SlotStoreInMemory implements SlotStore {
     }
 
     public Optional<ProcessedSlot> tryToReserveSlot(long tenantId, LocalDate date, LocalTime time) {
-        Map<LocalTime, SlotData> daySlotsMap;
-        // locking the main map for a short period of time
-        synchronized (mapDayToSlots) {
-            daySlotsMap = mapDayToSlots.computeIfAbsent(date, k -> new ConcurrentHashMap<>());
-        }
 
-        //locking only one day map
-        synchronized (daySlotsMap) {
-            if (!daySlotsMap.containsKey(time)) {
-                SlotData slot = new SlotData(time, SlotStatus.WAITING_APPROVAL, tenantId);
-                daySlotsMap.put(time, slot);
-                return Optional.of(new ProcessedSlot(slot.getStatus(), slot.getTenantId()));
-            } else
-                return Optional.empty();
+        var daySlotsMap = mapDayToSlots.computeIfAbsent(date, k -> new ConcurrentHashMap<>());
+
+        var prevSlot = daySlotsMap.putIfAbsent(time, new SlotData(time, SlotStatus.WAITING_APPROVAL, tenantId));
+
+        if (prevSlot != null)
+            // put was rejected
+            return Optional.empty();
+        else {
+            // getting new slot
+            var slot = daySlotsMap.get(time);
+            return Optional.of(new ProcessedSlot(slot.getStatus(), slot.getTenantId()));
         }
     }
 
     public Optional<ProcessedSlot> tryToCancelSlot(long tenantId, LocalDate date, LocalTime time) {
-        Map<LocalTime, SlotData> daySlotsMap = mapDayToSlots.computeIfAbsent(date, k -> new ConcurrentHashMap<>());
-        SlotData slot = daySlotsMap.get(time);
-        if (slot != null) {
-            if (slot.getTenantId() == tenantId && slot.getStatus() != SlotStatus.REJECTED) {
-                daySlotsMap.remove(time);
-                return Optional.of(new ProcessedSlot(SlotStatus.CANCELLED, slot.getTenantId()));
-            }
-        }
-        return Optional.empty();
+        var daySlotsMap = mapDayToSlots.get(date);
+
+        if (daySlotsMap != null) {
+            var slot = daySlotsMap.computeIfPresent(time, (timeKey, slotData) -> {
+                if (slotData.getTenantId() == tenantId && slotData.getStatus() != SlotStatus.REJECTED) {
+                    //removing element
+                    return null;
+                } else
+                    return slotData;
+            });
+
+            if (slot == null) // slot removed
+                return Optional.of(new ProcessedSlot(SlotStatus.CANCELLED, tenantId));
+            else
+                return Optional.empty();
+        } else
+            return Optional.empty();
     }
 
     @Override
@@ -70,27 +75,19 @@ public class SlotStoreInMemory implements SlotStore {
     }
 
     private Optional<ProcessedSlot> changeSlotStatusFromWaiting(SlotStatus newSlotStatus, LocalDate date, LocalTime time) {
-        return findAndChangeSlot(date, time, (mayBeSlot) -> {
-            if (mayBeSlot.isPresent()) {
-                SlotData slot = mayBeSlot.get();
-                if (slot.getStatus() == SlotStatus.WAITING_APPROVAL) {
-                    slot.setStatus(newSlotStatus);
-                    return new ProcessedSlot(newSlotStatus, slot.getTenantId());
-                }
-            }
-
-            return null;
-        });
-    }
-
-
-    private Optional<ProcessedSlot> findAndChangeSlot(LocalDate date, LocalTime time,
-                                                      Function<Optional<SlotData>, ProcessedSlot> slotAction) {
         Map<LocalTime, SlotData> daySlotsMap = mapDayToSlots.getOrDefault(date, Collections.emptyMap());
         SlotData slot = daySlotsMap.get(time);
-        return Optional.ofNullable(
-                slotAction.apply(
-                        Optional.ofNullable(slot)));
+
+        if (slot != null) {
+            synchronized (slot) {
+                if (slot.getStatus() == SlotStatus.WAITING_APPROVAL) {
+                    slot.setStatus(newSlotStatus);
+                    return Optional.of(new ProcessedSlot(newSlotStatus, slot.getTenantId()));
+                }
+            }
+        }
+
+        return Optional.empty();
     }
 
     @Override
